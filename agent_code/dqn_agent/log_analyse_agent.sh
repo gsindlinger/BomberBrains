163dede1c9e7320f5b6506b06a9816026f3bc74d
dkeyGit <flip@knorr-online.de>
2023-09-26 13:38:16 +0200

    log-file updated

--- agent_code/dqn_agent/log_analyse_agent.sh
+++ agent_code/dqn_agent/log_analyse_agent.sh
@@ -1,31 +1,39 @@
 #! /bin/bash
+# Works on Linux Fedora37 and NOT on OSX !? (grep)
 if (($# != 1))
-    then echo "Usage: $0 logfile"
-    exit 1
+   then echo "Usage: $0 logfile"
+   exit 1
 fi
 
 echo "Analyse Logfile:" $1
 
+WORKFILE=$1".work"
+echo "Analyse workfile:" $WORKFILE
+STATISTICS_FILE=$1".statistics"
+echo "Analyse statistics file:" $STATISTICS_FILE
+
+# Correction Log (end_of_round)
+#      DEBUG: end_of_round Encountered event(s) 'INVALID_ACTION'
+# --> #DEBUG: Encountered game event(s) 'INVALID_ACTION'
+cat $1 | sed "s/DEBUG: end_of_round Encountered event/DEBUG: Encountered game event/g" > $WORKFILE
+
 # -----------------------------------
 #  Analyse: Encountered game event(s)
 # -----------------------------------
-#2023-09-23 16:57:24,497 [best_ruehl_code] DEBUG: Encountered game event(s) 'INVALID_ACTION' in step 5
-
-grep -Eo '(?<='Encountered\ game\ event...')[^in]*' $1 | sed 's/,/\n/g' | sed "s/'//g" | awk ' 
-     {if (NR == 1){
-        print "\nEVENT\t COUNT" # > "fileresult.txt";
-     }
-     count[$1]++} END {for (word in count) print count[word], "\t", word
-     }'
+grep -Po '(?<='Encountered\ game\ event...')[^in]*' $WORKFILE | sed 's/,/\n/g' | sed "s/'//g" | awk -v file=$STATISTICS_FILE ' 
+    {if (NR == 1){
+       print "\nEVENT\t COUNT" > file;
+    }
+    count[$1]++} END {for (word in count) print count[word], "\t", word  >> file;
+    }'
 
 
 # ----------------------------------
 #  Analyse: Action via Model
 # ----------------------------------
-#2023-09-23 16:57:24,497 [best_ruehl_code] DEBUG: Encountered game event(s) 'INVALID_ACTION' in step 5
-grep -Eo '.*Action via Model:.*' $1 | uniq -c | awk ' 
-     {if (NR == 1){
-        print "\nAKTION\t COUNT"  # > "fileresult.txt";
-     }
-     count[$9]++} END {for (word in count) print word, "\t", count[word]
-     }'
+grep -Eo '.*Action via Model:.*' $WORKFILE | uniq -c | awk -v file=$STATISTICS_FILE ' 
+    {if (NR == 1){
+       print "\nAKTION\t COUNT" word >> file;
+    }
+    count[$9]++} END {for (word in count) print word, "\t", count[word] >> file;
+    }'
